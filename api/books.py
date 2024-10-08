4ac27b1a84ef6ae0a188c17ccf4705b3e5382a14
farooq-teqniqly <farooq@teqniqly.com>
2024-10-08 16:32:53 -0700

    Add API title and new book to the catalog
    Updated the FastAPI app instantiation to include a title "My Books API". Also added the book "Go Set a Watchman" by Harper Lee to the existing catalog.

--- api/books.py
+++ api/books.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="My Books API")
 
 BOOKS = [
-    dict(title="Foo", author="Bar", category="Science"),
     dict(title="The Great Gatsby", author="F. Scott Fitzgerald", category="Fiction"),
     dict(title="1984", author="George Orwell", category="Dystopian"),
     dict(title="To Kill a Mockingbird", author="Harper Lee", category="Classic"),
     dict(title="A Brief History of Time", author="Stephen Hawking", category="Science"),
     dict(title="The Catcher in the Rye", author="J.D. Salinger", category="Fiction"),
+    dict(title="Go Set a Watchman", author="Harper Lee", category="Classic"),
 ]
 
 
