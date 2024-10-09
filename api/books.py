82c25428ed367e8fdaaac9dafeba75a5bf976d0a
farooq-teqniqly <farooq@teqniqly.com>
2024-10-08 17:12:42 -0700

    Add book search by ISBN and refactor data management
    Implemented a new endpoint to retrieve books by their ISBN. Moved book data to a separate module for better code organization and updated the book list with ISBNs. Added author last name sorting to the main list endpoint for improved usability.

--- api/books.py
+++ api/books.py
@@ -1,17 +1,25 @@
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI(title="My Books API")
+from api.data import BOOKS
 
-BOOKS = [
-    dict(title="The Great Gatsby", author="F. Scott Fitzgerald", category="Fiction"),
-    dict(title="1984", author="George Orwell", category="Dystopian"),
-    dict(title="To Kill a Mockingbird", author="Harper Lee", category="Classic"),
-    dict(title="A Brief History of Time", author="Stephen Hawking", category="Science"),
-    dict(title="The Catcher in the Rye", author="J.D. Salinger", category="Fiction"),
-    dict(title="Go Set a Watchman", author="Harper Lee", category="Classic"),
-]
+app = FastAPI(title="My Books API")
 
 
 @app.get("/books")
 async def get_all_books():
+    BOOKS.sort(key=lambda b: _get_author_last_name(b["author"]))
     return BOOKS
+
+
+@app.get("/books/{isbn}")
+async def get_book(isbn: str):
+    return [b for b in BOOKS if b.get("isbn") == isbn]
+
+
+def _get_author_last_name(name: str) -> str:
+    return name.split()[-1]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ api/data.py
@@ -0,0 +1,35 @@
+BOOKS = [
+    dict(
+        title="A Brief History of Time",
+        author="Stephen Hawking",
+        category="Science",
+        isbn="9780553380163",
+    ),
+    dict(
+        title="The Great Gatsby",
+        author="F. Scott Fitzgerald",
+        category="Classic",
+        isbn="9780743273565",
+    ),
+    dict(
+        title="1984", author="George Orwell", category="Dystopian", isbn="9780451524935"
+    ),
+    dict(
+        title="To Kill a Mockingbird",
+        author="Harper Lee",
+        category="Classic",
+        isbn="9780061120084",
+    ),
+    dict(
+        title="The Catcher in the Rye",
+        author="J.D. Salinger",
+        category="Fiction",
+        isbn="9780316769488",
+    ),
+    dict(
+        title="Go Set a Watchman",
+        author="Harper Lee",
+        category="Classic",
+        isbn="9780062409850",
+    ),
+]
