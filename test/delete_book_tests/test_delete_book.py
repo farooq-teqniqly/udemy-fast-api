6eb4908c362ba51fd0cdbe048620358720cea6e3
farooq-teqniqly <farooq@teqniqly.com>
2024-10-12 00:06:27 -0700

    Refactor delete book tests for clarity and thoroughness
    Refactored existing tests to use constants for ISBNs, improving readability and maintainability. Added new tests to ensure proper handling of non-existent books and validation of the 'soft_deleted' flag. Enhanced mocking setup for better isolation and accuracy in tests.

--- test/delete_book_tests/test_delete_book.py
+++ test/delete_book_tests/test_delete_book.py
@@ -4,12 +4,55 @@ This module contains test cases for the DELETE /books/{isbn} endpoint of the API
 
 from test import client
 
+VALID_ISBN = "4444444444444"
+INVALID_ISBN = "foobar1234"
+NON_EXISTENT_ISBN = "0000000000000"
+
+
+def _setup_mock_books(mocker, books):
+    mocker.patch("api.book_service.BOOKS", books)
+
 
 def test_delete_book_fails_with_invalid_isbn_in_url():
-    response = client.delete("/books/foobar1234")
+    response = client.delete(f"/books/{INVALID_ISBN}")
     assert response.status_code == 422
 
 
-def test_delete_book_succeeds_with_valid_isbn_in_url():
-    response = client.delete("/books/4444444444444")
+def test_delete_book_when_successful_sets_soft_deleted_to_true(mocker):
+    mock_books = [
+        dict(
+            isbn=VALID_ISBN,
+            title="A Brief History of Time",
+            author="Stephen Hawking",
+            category="Science",
+            avg_rating=None,
+            num_ratings=0,
+            sum_ratings=0,
+            soft_deleted=False,
+        )
+    ]
+
+    _setup_mock_books(mocker, mock_books)
+    response = client.delete(f"/books/{VALID_ISBN}")
+    assert response.status_code == 200
+    assert mock_books[0]["soft_deleted"]
+
+
+def test_delete_book_succeeds_even_when_book_does_not_exist(mocker):
+    mock_books = [
+        dict(
+            isbn=VALID_ISBN,
+            title="A Brief History of Time",
+            author="Stephen Hawking",
+            category="Science",
+            avg_rating=None,
+            num_ratings=0,
+            sum_ratings=0,
+            soft_deleted=False,
+        )
+    ]
+
+    _setup_mock_books(mocker, mock_books)
+    mocker.patch("api.book_service.BOOKS", mock_books)
+    response = client.delete(f"/books/{NON_EXISTENT_ISBN}")
     assert response.status_code == 200
