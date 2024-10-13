21aeee4641c75a5ee32e201a21b49c18a03be88b
farooq-teqniqly <farooq@teqniqly.com>
2024-10-12 17:13:49 -0700

    Add parameterized tests for invalid book creation requests
    Refactor test_create_book to use dictionary instead of model. Added parameterized tests to verify invalid requests yield UNPROCESSABLE_ENTITY status.

--- test/create_book_tests/test_create_book.py
+++ test/create_book_tests/test_create_book.py
@@ -1,7 +1,11 @@
+import json
 from test import client
 
+import pytest
+
 import api.http_status_codes as status_code
-from api.models import CreateBookRequest
+
+VALID_ISBN = "4444444444444"
 
 
 def _setup_mock_books(mocker, books):
@@ -12,21 +16,50 @@ def test_create_book_is_successful(mocker):
     mock_books = []
     _setup_mock_books(mocker, mock_books)
 
-    request = CreateBookRequest(
+    request = dict(
         title="The Great Gatsby",
         author="F. Scott Fitzgerald",
-        isbn="9780743273565",
+        isbn=VALID_ISBN,
         category="Classics",
     )
 
-    response = client.post("/books", json=request.model_dump())
+    response = client.post("/books", json=request)
     assert response.status_code == status_code.OK
 
     created_book = mock_books[0]
-    assert created_book["title"] == request.title
-    assert created_book["author"] == request.author
-    assert created_book["isbn"] == request.isbn
-    assert created_book["category"] == request.category
+    assert created_book["title"] == request["title"]
+    assert created_book["author"] == request["author"]
+    assert created_book["isbn"] == request["isbn"]
+    assert created_book["category"] == request["category"]
     assert created_book["avg_rating"] is None
     assert created_book["num_ratings"] is None
     assert created_book["soft_deleted"] is False
+
+
+@pytest.mark.parametrize(
+    ("title", "author", "isbn", "category"),
+    [
+        (None, "Stephen King", VALID_ISBN, "Horror"),
+        ("", "Stephen King", VALID_ISBN, "Horror"),
+        ("The Stand", None, VALID_ISBN, "Horror"),
+        ("The Stand", "", VALID_ISBN, "Horror"),
+        ("The Stand", "Stephen King", None, "Horror"),
+        ("The Stand", "Stephen King", "", "Horror"),
+        ("The Stand", "Stephen King", "fffffffff1111", "Horror"),
+        ("The Stand", "Stephen King", "fffffffffffff", "Horror"),
+        ("The Stand", "Stephen King", "11111111111111", "Horror"),
+        ("The Stand", "Stephen King", "111111111111", "Horror"),
+        ("The Stand", "Stephen King", VALID_ISBN, None),
+        ("The Stand", "Stephen King", VALID_ISBN, ""),
+    ],
+)
+def test_create_book_fails_with_invalid_request(title, author, isbn, category):
+    request = dict(
+        title=title,
+        author=author,
+        isbn=isbn,
+        category=category,
+    )
+
+    response = client.post("/books", json=json.dumps(request))
+    assert response.status_code == status_code.UNPROCESSABLE_ENTITY
