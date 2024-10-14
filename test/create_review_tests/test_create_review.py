567f7f31660d0437176462644a91706258bc92c0
farooq-teqniqly <farooq@teqniqly.com>
2024-10-14 09:13:45 -0700

    Add test for create reviews with invalid ISBNs
    This commit introduces a new parameterized test to ensure that creating reviews with invalid ISBNs fails as expected. The new test checks that the API returns an UNPROCESSABLE_ENTITY status code when invalid ISBNs are used.

--- test/create_review_tests/test_create_review.py
+++ test/create_review_tests/test_create_review.py
@@ -1,5 +1,6 @@
 from test import client
 
+import pytest
 import test_data
 
 import api.http_status_codes as status_code
@@ -13,3 +14,10 @@ def test_can_add_review_to_book(mocker):
     response = client.post(f"/books/{test_data.VALID_ISBN}/reviews", json=request)
     assert response.status_code == status_code.OK
     assert any(review_text in review_data["reviews"] for review_data in mock_reviews)
+
+
+@pytest.mark.parametrize("isbn", test_data.INVALID_ISBNS)
+def test_create_reviews_fails_when_isbn_invalid(isbn):
+    request = dict(review="Yay!")
+    response = client.post(f"/books/{isbn}/reviews", json=request)
+    assert response.status_code == status_code.UNPROCESSABLE_ENTITY
